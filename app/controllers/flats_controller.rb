6c90b7ceaa91441f5a85a2f7605eb0579e4144cd
André Marques <115092696+CharlesSwannn@users.noreply.github.com>
2023-02-22 15:48:46 +0000

    implement all the necessary steps for a browsable simple airbnb

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,5 +1,5 @@
 class FlatsController < ApplicationController
-  before_action :set_flat, only: %i[show]
+  before_action :set_flat, only: %i[show edit update destroy]
 
   def index
     @flats = Flat.all
@@ -8,7 +8,37 @@ class FlatsController < ApplicationController
   def show
   end
 
+  def new
+    @flat = Flat.new
+  end
+
+  def edit
+  end
+
+  def update
+    @flat.update(flat_params)
+    redirect_to flat_path(@flat)
+  end
+
+  def create
+    @flat = Flat.create(flat_params)
+    if @flat.save
+      redirect_to flat_path(@flat)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @flat.destroy
+    redirect_to flats_path, status: :see_other
+  end
+
   private
+
+  def flat_params
+    params.require(:flat).permit(:name, :address, :description, :number_of_guests, :price_per_night)
+  end
 
   def set_flat
     @flat = Flat.find(params[:id])
