668563e10a174a175001a1a5cae712e2de9787f3
André Marques <115092696+CharlesSwannn@users.noreply.github.com>
2023-02-22 13:03:28 +0000

    created seeds file and seeded db

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,47 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+puts "Destroying previous flats"
+puts "--------------------------"
+sleep(1)
+
+Flat.destroy_all
+
+puts "Creating new flats"
+puts "--------------------------"
+sleep(1)
+flat = Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+puts "#{flat.name} was created successfully"
+sleep(1)
+flat = Flat.create!(
+  name: 'Uma casinha bem jeitosa',
+  address: 'Marquês do Pombal',
+  description: 'Muito aconchegante',
+  price_per_night: 45,
+  number_of_guests: 2
+)
+puts "#{flat.name} was created successfully"
+sleep(1)
+flat = Flat.create!(
+  name: "Ronaldo's house",
+  address: 'Saldanha',
+  description: "It is Ronaldo's house. SIIIIIIIM",
+  price_per_night: 750,
+  number_of_guests: 20
+)
+puts "#{flat.name} was created successfully"
+sleep(1)
+flat = Flat.create!(
+  name: 'Pardieiro de Alvalade',
+  address: 'Perto do Sto António',
+  description: 'Really bad apartment',
+  price_per_night: 809,
+  number_of_guests: 1
+)
+puts "#{flat.name} was created successfully"
+sleep(1)
+puts "All done"
+puts "--------------------------"
